630aa1918a5d1360d27bac06dae455ea960693dc
Sai_Chen <saikaungsanchen@gmail.com>
2026-01-01 18:18:29 -0500

    feature: add error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,10 +28,16 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = Math.round(amount * 100);
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
+    try{
+        await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date});
   `;
+    }
+    catch (error){
+        console.error('Database Error:', error);
+        return {message: 'Database Insertion Failed' };
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 
@@ -46,11 +52,16 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
+  try{
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  }catch (error){
+    console.error('Database Error:', error);
+    return {message: 'Database Update Failed' };
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
